30ff9a30d1ff878b0932b4fa2ea74a247060613e
Arnobrizwan <arnobrizwan23@gmail.com>
2024-12-16 21:29:15 +0800

    c

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -9,6 +9,8 @@ import VerifyEmail from "./auth/VerifyEmail";
 import HereSection from './components/HereSection';
 import Profile from './components/Profile';
 import SearchPage from './components/SearchPage';
+import RestaurantDetail from './components/RestaurantDetail';
+import Cart from './components/cart';
 
 const appRouter = createBrowserRouter([
   {
@@ -26,6 +28,14 @@ const appRouter = createBrowserRouter([
       {
         path:"/search/:text",
         element:<SearchPage/>
+      },
+      {
+        path:"/restaurant/:id",
+        element:<RestaurantDetail/>
+      },
+      {
+        path:"/cart",
+        element:<Cart/>
       },
 
     ],
