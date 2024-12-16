d030374defbf179ec505f44836ce674bb31ec280
Arnobrizwan <arnobrizwan23@gmail.com>
2024-12-16 20:46:53 +0800

    Update RestaurantDetail.tsx

--- Client/src/components/RestaurantDetail.tsx
+++ Client/src/components/RestaurantDetail.tsx
@@ -91,9 +91,8 @@ const RestaurantDetail = () => {
               </div>
             </div>
           </div>
-          {singleRestaurant?.menus && (
-            <AvailableMenu menus={singleRestaurant?.menus} />
-          )}
+        
+
         </div>
       )}
     </div>
