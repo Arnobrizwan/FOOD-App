6d7b993bad756b8f042a7eefbefb0dd3b90f2a56
Arnobrizwan <arnobrizwan23@gmail.com>
2024-12-16 18:06:04 +0800

    c

--- Client/src/components/Footer.tsx
+++ Client/src/components/Footer.tsx
@@ -1,11 +1,10 @@
 const Footer = () => {
     return (
-        <div>Footer</div>
-      /*<footer className="bg-gray-800 text-center text-gray-300 py-8 px-4">
+        <footer className="bg-gray-800 text-center text-gray-300 py-8 px-4">
         <p className="text-sm">
             &copy; 2024 FoodTiger. All rights reserved.
           </p> 
-      </footer>*/
+      </footer>
     );
   };
   
