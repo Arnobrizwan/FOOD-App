7eb370779d8097dc9c960575ebacc16be857a89e
Arnobrizwan <arnobrizwan23@gmail.com>
2024-12-16 21:22:27 +0800

    c

--- Client/src/auth/ForgetPassword.tsx
+++ Client/src/auth/ForgetPassword.tsx
@@ -4,7 +4,7 @@ import { Loader2, Mail } from "lucide-react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-const ForgetPassword = () => {
+const ForgotPassword = () => {
     const [email, setEmail] = useState<string>("");
     const loading =  false;
 
@@ -12,7 +12,7 @@ const ForgetPassword = () => {
     <div className="flex items-center justify-center min-h-screen w-full">
       <form className="flex flex-col gap-5 md:p-8 w-full max-w-md rounded-lg mx-4">
         <div className="text-center">
-          <h1 className="font-extrabold text-2xl mb-2">Forget Password</h1>
+          <h1 className="font-extrabold text-2xl mb-2">Forgot Password</h1>
           <p className="text-sm text-gray-600">Enter your email address to reset your password</p>
         </div>
         <div className="relative w-full">
@@ -41,4 +41,4 @@ const ForgetPassword = () => {
   );
 };
 
-export default ForgetPassword;
+export default ForgotPassword;
