d38967a3aacb67c4e47599c62d05238f18bbd7e5
Arnobrizwan <arnobrizwan23@gmail.com>
2024-12-17 10:54:06 +0800

    Update index.ts

--- Server/index.ts
+++ Server/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 const app = express();
 
-const PORT =process.env.PORT || 3005;
+const PORT =process.env.PORT || 3009;
 app.listen(PORT, () =>{
     console.log(`Server listen at port ${PORT}`);
 })
