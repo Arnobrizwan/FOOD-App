d246864d9448a9b754b7c12ccd87ba5fdcbeca9b
Arnobrizwan <arnobrizwan23@gmail.com>
2024-12-17 11:04:06 +0800

    c

--- Server/index.ts
+++ Server/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
 const app = express();
 
 const PORT =process.env.PORT || 3009;
+console.log("Loaded PORT:", process.env.PORT); // Debugging line
 app.listen(PORT, () =>{
     console.log(`Server listen at port ${PORT}`);
 })
