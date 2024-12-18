0c4b4ef5951bda8937cc7337856769839cf0587e
Arnobrizwan <arnobrizwan23@gmail.com>
2024-12-18 08:20:59 +0800

    Update mailtrap.ts
    c

--- Server/mailtrap/mailtrap.ts
+++ Server/mailtrap/mailtrap.ts
@@ -7,7 +7,7 @@ export const client = new MailtrapClient({token: process.env.MAILTRAP_API_TOKEN!
 
 
 
-const sender = {
+export const sender = {
   email: "hello@demomailtrap.com",
   name: "Arnob Rizwan",
 };
